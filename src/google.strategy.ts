f99b214ebd494f1bb91011cce8ef35f1166f8fda
janviramani00 <ramanijanvi00@gmail.com>
2023-01-11 16:47:08 +0530

    first commit

---
+++ src/google.strategy.ts
@@ -0,0 +1,36 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { config } from 'dotenv';
+
+import { Injectable } from '@nestjs/common';
+
+config();
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+  constructor() {
+    super({
+      clientID:
+        '627614554620-1fno2j6vg18006hkpjv8ea8dfnaq6q4h.apps.googleusercontent.com',
+      clientSecret: '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',
+      callbackURL: 'http://localhost:5000/auth/google/callback',
+      scope: ['email', 'profile'],
+    });
+  }
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: any,
+    done: VerifyCallback,
+  ): Promise<any> {
+    const { name, emails, photos } = profile;
+    const user = {
+      email: emails[0].value,
+      firstName: name.givenName,
+      lastName: name.familyName,
+      picture: photos[0].value,
+      accessToken,
+    };
+    done(null, user);
+  }
+}
